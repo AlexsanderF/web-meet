3a2891dc9f9b8fd008c93731dfdcd2c4fc3840f0
Alexsander Fontes <sander-369@hotmail.com>
2025-01-30 18:19:00 -0300

    :sparkles: feat: Creation of the option to enter the room for interactions and leave the room for interactions.

--- js/room_rtc.js
+++ js/room_rtc.js
@@ -101,12 +101,12 @@ let joinRoomInit = async () => {
 
     client.on('user-published', handleUserPublished);
     client.on('user-left', handleUserLeft);
-
-    // Iniciar a transmissão local
-    joinStream();
 };
 
 let joinStream = async () => {
+    document.getElementById('join-btn').style.display = 'none';
+    document.getElementsByClassName('stream__actions')[0].style.display = 'flex';
+
     try {
         localTrack = await AgoraRTC.createMicrophoneAndCameraTracks({}, {
             encoderConfig: {
@@ -178,7 +178,11 @@ let handleUserPublished = async (user, mediaType) => {
 
 let handleUserLeft = async (user) => {
     delete remoteUsers[user.uid];
-    document.getElementById(`user-container-${user.uid}`).remove();
+    let item = document.getElementById(`user-container-${user.uid}`);
+
+    if (item) {
+        item.remove();
+    }
 
     if (userIDInDisplayFrame === `user-container-${user.uid}`) {
         displayFrame.style.display = null;
@@ -263,8 +267,41 @@ let toggleScreen = async (e) => {
     }
 }
 
+let leaveStream = async (e) => {
+    e.preventDefault();
+
+    document.getElementById('join-btn').style.display = 'block';
+    document.getElementsByClassName('stream__actions')[0].style.display = 'none';
+
+    for (let i = 0; localTracks.length > i; i++) {
+        localTracks[i].stop();
+        localTracks[i].close();
+    }
+
+    await client.unpublish([localTracks[0], localTracks[1]]);
+
+    if (localScreenTracks) {
+        await client.unpublish([localScreenTracks]);
+    }
+
+    document.getElementById(`user-container-${uid}`).remove();
+
+    if (userIDInDisplayFrame === `user-container-${uid}`) {
+        displayFrame.style.display = null;
+
+        for (let i = 0; videoFrames.length > i; i++) {
+            videoFrames[i].style.height = '300px';
+            videoFrames[i].style.width = '300px';
+        }
+    }
+
+    channel.sendMessage({text: JSON.stringify({'type': 'user_left', 'uid': uid})});
+}
+
 document.getElementById('camera-btn').addEventListener('click', toggleCamera);
 document.getElementById('mic-btn').addEventListener('click', toggleMic);
 document.getElementById('screen-btn').addEventListener('click', toggleScreen);
+document.getElementById('leave-btn').addEventListener('click', leaveStream);
+document.getElementById('join-btn').addEventListener('click', joinStream);
 
 joinRoomInit();
